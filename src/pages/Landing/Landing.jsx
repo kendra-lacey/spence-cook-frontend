c7d92ab7f83840582b33cc027dfdb49c97bac5f2
Cazier92 <bryce.cazier@gmail.com>
2023-09-18 16:03:48 -0700

    add splash image to landing, update styling to work better with nav and footer.

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -4,7 +4,7 @@ import styles from './Landing.module.css'
 const Landing = () => {
   return (
     <main className={styles.container}>
-      
+      <img src="./SpenceCookLanding.png" alt="Stylized image of musician Spence Cook" className={styles.landingImg}/>
     </main>
   )
 }
