6eef3e6bc5d557bd8cd64a705008483a5ef4bcc8
Cazier92 <bryce.cazier@gmail.com>
2023-09-21 23:42:01 -0700

    update bookings page

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -9,8 +9,8 @@ const Bookings = () => {
       </div>
       <div id='book-body-container'>
         <div id="blob-book">
-          <p>Do you have questions, want to book a show, or collaborate on a new piece? Reach out, and let's make music happen.</p>
-          <img id='book-loc' src="./location.png" alt="" />
+          {/* <p>Do you have questions, want to book a show, or collaborate on a new piece? Reach out, and let's make music happen.</p>
+          <img id='book-loc' src="./location.png" alt="" /> */}
         </div>
         <a href="mailto:SPENCE@SPENCESINGS.com"><img id='book-page-btn' src="./bookNowBtn.png" alt="" /></a>
       </div>
