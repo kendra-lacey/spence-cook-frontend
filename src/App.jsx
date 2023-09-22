b8efd2cbcedf1b5a22ce1eb4286a233c9a882de7
Cazier92 <bryce.cazier@gmail.com>
2023-09-22 06:48:58 -0700

    add images for album page

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,22 @@ import { Routes, Route,
 } from 'react-router-dom'
 
 // pages
-// import Signup from './pages/Signup/Signup'
-// import Login from './pages/Login/Login'
 import Landing from './pages/Landing/Landing'
 import About from './pages/About/About'
 import Bookings from './pages/Bookings/Bookings'
 import Album from './pages/Album/Album'
-// import Profiles from './pages/Profiles/Profiles'
-// import ChangePassword from './pages/ChangePassword/ChangePassword'
 
 // components
 import NavBar from './components/NavBar/NavBar'
 import Footer from './components/Footer/Footer'
-// import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute'
 
-// services
-// import * as authService from './services/authService'
+
+
 
 // styles
 import './App.css'
 
 function App() {
-  // const [user, setUser] = useState(authService.getUser())
-  // const navigate = useNavigate()
-
-  // const handleLogout = () => {
-  //   authService.logout()
-  //   setUser(null)
-  //   navigate('/')
-  // }
-
-  // const handleAuthEvt = () => {
-  //   setUser(authService.getUser())
-  // }
 
   return (
     <>
