1b785443ecb5e3f2ecddc63a206d06aaefc9443e
Cazier92 <bryce.cazier@gmail.com>
2023-09-23 13:43:11 -0700

    stub up album page

--- src/pages/Album/Album.jsx
+++ src/pages/Album/Album.jsx
@@ -3,8 +3,22 @@ import './Album.css'
 
 const Album = () => {
   return ( 
-    <main>
-      <h1>Album Page Here</h1>
+    <main id='album-main'>
+      <div id='embed-div'>
+        <h1>Rearview</h1>
+        <p>Released in November 2022</p>
+        <div></div>
+        <div id='media-links'></div>
+      </div>
+      <div id='about-album-container'>
+        <h1>Behind the music...</h1>
+        <div id='album-carousel'></div>
+        <div id='album-info'>
+          <p>In early 2020, Spence resolved to create his debut album. Then the pandemic struck, prompting deep contemplation during quarantine.
+          <br />
+          "Rearview" delves into midlife, where you ponder the years passed, grasp aging's reality, and cherish the preciousness of time, assessing past, present, and future endeavors.</p>
+        </div>
+      </div>
     </ main>
   );
 }
