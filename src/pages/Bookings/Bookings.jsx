faeefe89c6d7707dfac25d4d6ab60cfb866e6ff4
Cazier92 <bryce.cazier@gmail.com>
2023-09-24 00:19:45 -0700

    styling for mobile bookings page complete

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -5,14 +5,14 @@ const Bookings = () => {
   return ( 
     <main id='book-main'>
       <div id='book-back-container'>
-        <div id='book-back-nav'></div>
+        {/* <div id='book-back-nav'></div> */}
       </div>
       <div id='book-body-container'>
         <div id="blob-book">
           {/* <p>Do you have questions, want to book a show, or collaborate on a new piece? Reach out, and let's make music happen.</p>
           <img id='book-loc' src="./location.png" alt="" /> */}
         </div>
-        <a href="mailto:SPENCE@SPENCESINGS.com"><img id='book-page-btn' src="./bookNowBtn.png" alt="" /></a>
+        <a id='book-a' href="mailto:SPENCE@SPENCESINGS.com"><img id='book-page-btn' src="./bookNowBtn.png" alt="" /></a>
       </div>
     </ main>
   );
