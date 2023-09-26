db0bb8eca20f9d6e090dfb132af132ece138e3d6
Cazier92 <bryce.cazier@gmail.com>
2023-09-26 04:10:44 -0700

    update social & music link icons to svg, update corresponding containers

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -10,16 +10,16 @@ const Landing = () => {
         <p id='splashTxt'>Life's detour led Spence away from his singing dreams, but mid-life served as his GPS back to the stage.</p>
         <div id='musicLink-container'>
           <a href="https://open.spotify.com/album/0oynh8lZzhbRoI01aR1hCv">
-            <img className='musicLink' src="./Spotify.png" alt="" />
+            <img className='musicLink' src="./Spotify.svg" alt="" />
           </a>
           <a href="https://www.pandora.com/artist/spence-cook/rearview/ALbbbw7dl5n6qqP">
-            <img className='musicLink' src="./pandora.png" alt="" />
+            <img className='musicLink' src="./pandora.svg" alt="" />
           </a>
           <a href="https://www.amazon.com/dp/B0BNL23527/ref=sr_1_4?keywords=spence+cook+rearview&qid=1670773727&sprefix=Spence+Cook+%2Caps%2C69&sr=8-4">
-            <img className='musicLink' src="./amazonMusic.png" alt="" />
+            <img className='musicLink' src="./amazonMusic.svg" alt="" />
           </a>
           <a href="https://www.iheart.com/artist/spence-cook-39264727/">
-            <img className='iHeartLink' src="./iHeart.png" alt="" />
+            <img className='musicLink' src="./iHeart.svg" alt="" />
           </a>
         </div>
         <div>
